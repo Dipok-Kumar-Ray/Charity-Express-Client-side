1830b78338116a5f31e6aa18406f2695b8f3da61
Dipok Kumar Ray <dipok7316@gmail.com>
2025-07-09 14:23:04 +0600

    Google Login implementation

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import useAuth from '../hooks/useAuth';
-import { Navigate, useLocation } from 'react-router';
+import React from "react";
+import useAuth from "../hooks/useAuth";
+import { Navigate, useLocation } from "react-router";
 
-const PrivateRoute = ({children}) => {
-    const {user, loading} = useAuth();
-    const location = useLocation();
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
 
-    if(loading){
-        return <span className='loading loading-spinner loading-xl'></span>
-    }
+  if (loading) {
+    return <span className="loading loading-spinner loading-xl"></span>;
+  }
 
-    if(!user){
-        return <Navigate state={{from: location.pathname}} to='/login'></Navigate>
-    }
+  if (!user) {
+    return (
+      <Navigate state={{ from: location.pathname }} to="/login"></Navigate>
+    );
+  }
 
-    return children;
+  return children;
 };
 
 export default PrivateRoute;
